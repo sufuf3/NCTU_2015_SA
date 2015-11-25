ac55a65c77154ef82a08ed834b7b76db47f45243
Shan Jung Fu <sufuf3@gmail.com>
2015-11-25 23:47:00 +0800

    Update zfs_snapshot.py

--- zfs_snapshot.py
+++ zfs_snapshot.py
@@ -6,11 +6,11 @@ import subprocess, re
 
 CURRENT = datetime.strftime(datetime.now(), '%Y%m%d_%H%M')
 
-sasnapshot = subprocess.check_output('sudo zfs snapshot mypool/ftp@sademo_'+CURRENT , shell=True, stderr=subprocess.STDOUT)
+sasnapshot = subprocess.check_output('zfs snapshot mypool/ftp@sademo_'+CURRENT , shell=True, stderr=subprocess.STDOUT)
 
 listcheck = subprocess.check_output('zfs list -t snapshot' , shell=True, stderr=subprocess.STDOUT)
 
 if len(listcheck.split('\n')) > 9:
     deletesnap =  listcheck.split('\n')[1].split(' ')[0]
-    byebye = subprocess.check_output('sudo zfs destroy '+deletesnap , shell=True, stderr=subprocess.STDOUT)
+    byebye = subprocess.check_output('zfs destroy '+deletesnap , shell=True, stderr=subprocess.STDOUT)
     print deletesnap + ' at ' + CURRENT + 'is gone.'
